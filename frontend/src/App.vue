8a2c5a4111733cc4d342ea065143d899c586800e
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-12-31 03:38:02 -0300

    Style(Navbar)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,24 +21,21 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  background: black;
+  color: white;
+  width: 100%;
+  height: 100vh;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,26 +1,92 @@
 <template>
     <header class="navbar">
         <nav>
-            <div class="avmb">
-
+            <div class="titulo">
+                <p>
+                    Minha Livraria
+                </p>
+            </div>
+            <div class="links">
+                <router-link to="/">
+                    Inicio
+                </router-link>
+                <router-link to="/novoLivro">
+                    Adicionar Livro
+                </router-link>
             </div>
         </nav>
-        <div class="links">
-            <router-link to="/">
-                Inicio
-            </router-link>
-        </div>
-        <router-link to="/novoLivro">
-            Adicionar Livro
-        </router-link>
     </header>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
-    export default defineComponent({
-        name: "NavBar"
-    })
+export default defineComponent({
+    name: "NavBar"
+})
 
 </script>
+
+<style scoped lang="scss">
+.navbar {
+    width: 100%;
+
+    nav {
+        width: 100vw;
+        display: flex;
+        background-color: #002a66;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 20pt;
+        height: 10vh;
+        color: white;
+
+
+        .titulo {
+            display: flex;
+            align-items: center;
+            padding: 0 15px;
+
+            p {
+                margin-right: 5px;
+            }
+        }
+
+
+
+
+
+
+        .links {
+            display: flex;
+            gap: 15px;
+            padding: 0 15px;
+            a {
+                display: flex;
+                text-decoration: none;
+                color: inherit;
+                transition: 1s;
+                &:hover {
+                    transform: scale(1.1);
+                }
+            }
+        }
+    }
+}
+
+@media (min-width: 360px) {
+  .navbar {
+    nav {
+      font-size: 0.9rem;
+    }
+  }
+}
+
+@media (min-width: 768px) {
+  .navbar {
+    nav {
+      font-size: 1.7rem;
+    }
+  }
+}
+</style>
